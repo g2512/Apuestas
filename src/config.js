77e6d46532abb84c6e800ab9234e92ee38c1882c
Guillermo Ciancaglini <94321135+g2512@users.noreply.github.com>
2026-06-07 14:31:58 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-import BettingSystemABI from "./BettingSystem.json";
-
-export const CONTRACT_ADDRESS = "0xF20a4f79E7f9Bc73593b662bD7c1620F3e973559"; // dirección del contrato desplegado en Sepolia
-export const CONTRACT_ABI = BettingSystemABI;
